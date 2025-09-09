9f9026534c7e925ccda60875db7a2dafcc7903f6
tranduynhan02 <duynhantran171102@gmail.com>
2025-09-09 16:20:25 +0700

    create

---
+++ src/test/java/com/nhantd/homestay/HomestayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhantd.homestay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomestayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
